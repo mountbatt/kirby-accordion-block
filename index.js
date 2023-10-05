5961f4408918795e28d0e569b80805749b7ddba9
Tobias Battenberg <mountbatt@gmail.com>
2023-10-05 16:01:02 +0200

    update 1

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ panel.plugin("mountbatt/accordion-block", {
     accordion: `
       	<div @dblclick="open">
 		    <template v-if="content.accordion.length === 0">
-		    	<details open><summary>Details</summary>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</details>
+		    	<details open><summary v-text="$t('field.blocks.accordion.name')"></summary><span v-text="$t('field.blocks.accordion.empty')"></span></details>
 		    </template>
 		    <template v-else>
-						
 		        <details open v-for="item in content.accordion">
 						<summary v-if="item.summary">{{ item.summary }}</summary>
 		        <summary v-else>Details</summary>
 		        <div v-html="item.details" class="k-text"></div>
 		        </details>
+						<div v-text="content.accordion.schema"></div>
 		    </template>
 		</div>
     `
   }
-});
+}); 
--- index.php
+++ index.php
@@ -5,5 +5,32 @@ Kirby::plugin('mountbatt/accordion-block', [
   ],
   'snippets' => [
     'blocks/accordion' => __DIR__ . '/snippets/blocks/accordion.php'
+  ],'translations' => [
+    'en' => [
+      'field.blocks.accordion.empty' => 'Please add some entries into your accordion …',
+      'field.blocks.accordion.name' => 'Accordion',
+      'field.blocks.accordion.nameentries' => 'Akkordion entries',
+      'field.blocks.accordion.showasfaq' => 'Show as FAQ-Schema for Google?',
+      'field.blocks.accordion.question' => 'Question',
+       'field.blocks.accordion.answer' => 'Answer',
+       'field.blocks.accordion.openonload' => 'Open element on load?',
+       'field.blocks.accordion.open' => 'Open',
+       'field.blocks.accordion.desc_question' => 'Add title or question here …',
+        'field.blocks.accordion.desc_answer' => 'Add collapsed answer here …',
+    ],
+    'de' => [
+       'field.blocks.accordion.empty' => 'Bitte füge ein paar Einträge in Dein Akkordeon ein …',
+       'field.blocks.accordion.name' => 'Akkordeon',
+       'field.blocks.accordion.nameentries' => 'Akkordeon-Einträge',
+       'field.blocks.accordion.showasfaq' => 'Als FAQ-Schema für Google anzeigen?',
+       'field.blocks.accordion.question' => 'Frage',
+       'field.blocks.accordion.answer' => 'Antwort',
+       'field.blocks.accordion.openonload' => 'Element beim Start öffnen?',
+       'field.blocks.accordion.open' => 'Offen',
+       'field.blocks.accordion.desc_question' => 'Füge hier den Titel oder die Fragestellung ein …',
+       'field.blocks.accordion.desc_answer' => 'Füge hier die zugeklappte Antwort ein …',
+  
+    ],
+    // more languages
   ]
 ]);
